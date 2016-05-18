a213da23a136bedab5498bc00ca1ff9f4d41068f
Larry Eisenstein <larryeisenstein@gmail.com>
2016-05-17 23:08:02 -0400

    Add reference and PDf

--- SnagitPOC/Main.cs
+++ SnagitPOC/Main.cs
@@ -7,18 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SNAGITLib;
+
 
 namespace SnagitPOC
 {
     public partial class Main : Form
     {
+        public readonly ImageCaptureClass SnagImg;
+        private string _lastSavedFolder;
+
         public Main()
         {
             InitializeComponent();
+            SnagImg = new ImageCaptureClass();
         }
 
         private void btnCapture1_Click(object sender, EventArgs e)
         {
+            SnagImg.Input = snagImageInput.siiWindow;
+            SnagImg.AutoScrollOptions.AutoScrollMethod = snagAutoScrollMethod.sasmBoth;
+            SnagImg.AutoScrollOptions.Delay = 100;
+
+            SnagImg.AutoScrollOptions.StartingPosition = snagAutoScrollStartingPosition.sasspTop;
+            
+
+
+
+
+            SnagImg.Output = snagImageOutput.sioClipboard;
+            SnagImg.OutputImageFile.FileType = snagImageFileType.siftPNG;
+            SnagImg.OutputImageFile.Quality = 90;
+            SnagImg.OutputImageFile.ColorDepth = snagImageColorDepth.sicd24Bit;
+            SnagImg.OutputImageFile.FileSubType = snagImageFileSubType.sifstJTIF_444;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            try
+            {
+                SnagImg.Capture();
+            }
+            catch
+            {
+                System.Console.WriteLine(SnagImg.LastError.ToString());
+            }
 
         }
 
